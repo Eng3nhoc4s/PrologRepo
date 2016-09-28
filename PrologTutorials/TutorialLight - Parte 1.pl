151f8ed01d8e635fe6789bf29df17338b0e0d041
Rodrigo Reis <N-genhocas@hotmail.com>
2016-09-28 11:13:13 +0100

    Add files via upload

---
+++ PrologTutorials/TutorialLight - Parte 1.pl
@@ -0,0 +1,59 @@
+% *********************
+% Factos simples
+% *********************
+
+joao_canta.
+joao_muito_alto.
+
+% Esta a chover
+chove.
+
+% Escadote muito alto
+escadote_muito_alto.
+
+
+% *********************
+% Factos com argumentos
+% % *******************
+
+baixinho(joao).
+viu(joao, diana).
+
+
+% As mulheres
+mulher(diana).
+mulher(ausenda).
+
+%Os homens
+homem(joao).
+homem(pedro).
+homem(gonzaga).
+homem(gasosa).
+
+% As cidades
+cidade(napoles).
+cidade(luanda).
+cidade(cartagenaDasIndias).
+
+% As vilas
+vila(vilaVerde).
+
+% As aldeias
+aldeia(pitoesDasJunias).
+
+% O Joao viu a Diana em Napoles
+viu(joao,diana,napoles).
+% O Joao viu o Pedro em Pitoes das Junias
+viu(joao,pedro,pitoesDasJunias).
+% O Joao viu o Pedro em Vila Verde
+viu(joao,pedro,vilaVerde).
+% A Diana viu o Gonzaga em Cartagena das Içndias
+viu(diana,gonzaga,cartagenaDasIndias).
+% A Diana foi vista pelo Gasosa em Luanda
+viu(gasosa,diana,luanda).
+% A Diana viu o Joao em Napoles
+viu(diana,joao,napoles).
+
+
+
+
